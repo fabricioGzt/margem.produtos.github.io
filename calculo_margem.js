bebd5157d40919839b553eb3623cff69870eb610
fabricioGzt <75496428+fabricioGzt@users.noreply.github.com>
2021-05-12 14:32:42 -0300

    Update calculo_margem.js

--- calculo_margem.js
+++ calculo_margem.js
@@ -1,19 +1,19 @@
 function calcular() {
     // verificação de campos em branco 
 
-    if ((document.getElementById("preco").value == "") || (document.getElementById("custo").value == "") || (document.getElementById("icms").value == "")) {
+    if ((document.getElementById("campo3").value == "") || (document.getElementById("campo2").value == "") || (document.getElementById("campo1").value == "")) {
         alert("Campos obrigatórios não preenchidos!");
         AbortController
     } else {
 
         // Calculo valor 0%
-        var valor1 = parseFloat(document.getElementById("preco").value) /
-            parseFloat(document.getElementById("custo").value);
-        var a = document.getElementById("preco").value
-        var b = document.getElementById("preco").value * 0 / 100;
+        var valor1 = parseFloat(document.getElementById("campo3").value) /
+            parseFloat(document.getElementById("campo2").value);
+        var a = document.getElementById("campo3").value
+        var b = document.getElementById("campo3").value * 0 / 100;
         var re1 = a - b;
-        var c = re1 - (re1 * (0.0925 + document.getElementById("icms").value / 100));
-        var d = c - document.getElementById("custo").value;
+        var c = re1 - (re1 * (0.0925 + document.getElementById("campo1").value / 100));
+        var d = c - document.getElementById("campo2").value;
         var e = d / c * 100;
 
         document.getElementById("tela").innerHTML = valor1.toFixed(2);
@@ -32,11 +32,11 @@ function calcular() {
 
         // Calculo valor 2%
 
-        var a2 = document.getElementById("preco").value
-        var b2 = document.getElementById("preco").value * 2 / 100;
+        var a2 = document.getElementById("campo3").value
+        var b2 = document.getElementById("campo3").value * 2 / 100;
         var re2 = a2 - b2;
-        var c2 = re2 - (re2 * (0.0925 + document.getElementById("icms").value / 100));
-        var d2 = c2 - document.getElementById("custo").value;
+        var c2 = re2 - (re2 * (0.0925 + document.getElementById("campo1").value / 100));
+        var d2 = c2 - document.getElementById("campo2").value;
         var e2 = d2 / c2 * 100;
 
 
@@ -55,11 +55,11 @@ function calcular() {
 
         // Calculo valor 5%
 
-        var a3 = document.getElementById("preco").value
-        var b3 = document.getElementById("preco").value * 5 / 100;
+        var a3 = document.getElementById("campo3").value
+        var b3 = document.getElementById("campo3").value * 5 / 100;
         var re3 = a3 - b3;
-        var c3 = re3 - (re3 * (0.0925 + document.getElementById("icms").value / 100));
-        var d3 = c3 - document.getElementById("custo").value;
+        var c3 = re3 - (re3 * (0.0925 + document.getElementById("campo1").value / 100));
+        var d3 = c3 - document.getElementById("campo2").value;
         var e3 = d3 / c3 * 100;
 
 
@@ -78,11 +78,11 @@ function calcular() {
 
         // Calculo valor 10%
 
-        var a4 = document.getElementById("preco").value
-        var b4 = document.getElementById("preco").value * 10 / 100;
+        var a4 = document.getElementById("campo3").value
+        var b4 = document.getElementById("campo3").value * 10 / 100;
         var re4 = a4 - b4;
-        var c4 = re4 - (re4 * (0.0925 + document.getElementById("icms").value / 100));
-        var d4 = c4 - document.getElementById("custo").value;
+        var c4 = re4 - (re4 * (0.0925 + document.getElementById("campo1").value / 100));
+        var d4 = c4 - document.getElementById("campo2").value;
         var e4 = d4 / c4 * 100;
 
 
@@ -101,11 +101,11 @@ function calcular() {
 
         // Calculo valor 15%
 
-        var a5 = document.getElementById("preco").value
-        var b5 = document.getElementById("preco").value * 15 / 100;
+        var a5 = document.getElementById("campo3").value
+        var b5 = document.getElementById("campo3").value * 15 / 100;
         var re5 = a5 - b5;
-        var c5 = re5 - (re5 * (0.0925 + document.getElementById("icms").value / 100));
-        var d5 = c5 - document.getElementById("custo").value;
+        var c5 = re5 - (re5 * (0.0925 + document.getElementById("campo1").value / 100));
+        var d5 = c5 - document.getElementById("campo2").value;
         var e5 = d5 / c5 * 100;
 
 
@@ -124,11 +124,11 @@ function calcular() {
 
         // Calculo valor 20%
 
-        var a6 = document.getElementById("preco").value
-        var b6 = document.getElementById("preco").value * 20 / 100;
+        var a6 = document.getElementById("campo3").value
+        var b6 = document.getElementById("campo3").value * 20 / 100;
         var re6 = a6 - b6;
-        var c6 = re6 - (re6 * (0.0925 + document.getElementById("icms").value / 100));
-        var d6 = c6 - document.getElementById("custo").value;
+        var c6 = re6 - (re6 * (0.0925 + document.getElementById("campo1").value / 100));
+        var d6 = c6 - document.getElementById("campo2").value;
         var e6 = d6 / c6 * 100;
 
 
@@ -147,11 +147,11 @@ function calcular() {
 
         // Calculo valor 25%
 
-        var a7 = document.getElementById("preco").value
-        var b7 = document.getElementById("preco").value * 25 / 100;
+        var a7 = document.getElementById("campo3").value
+        var b7 = document.getElementById("campo3").value * 25 / 100;
         var re7 = a7 - b7;
-        var c7 = re7 - (re7 * (0.0925 + document.getElementById("icms").value / 100));
-        var d7 = c7 - document.getElementById("custo").value;
+        var c7 = re7 - (re7 * (0.0925 + document.getElementById("campo1").value / 100));
+        var d7 = c7 - document.getElementById("campo2").value;
         var e7 = d7 / c7 * 100;
 
 
@@ -171,11 +171,11 @@ function calcular() {
 
         // Calculo valor 28%
 
-        var a8 = document.getElementById("preco").value
-        var b8 = document.getElementById("preco").value * 28 / 100;
+        var a8 = document.getElementById("campo3").value
+        var b8 = document.getElementById("campo3").value * 28 / 100;
         var re8 = a8 - b8;
-        var c8 = re8 - (re8 * (0.0925 + document.getElementById("icms").value / 100));
-        var d8 = c8 - document.getElementById("custo").value;
+        var c8 = re8 - (re8 * (0.0925 + document.getElementById("campo1").value / 100));
+        var d8 = c8 - document.getElementById("campo2").value;
         var e8 = d8 / c8 * 100;
 
 
@@ -194,11 +194,11 @@ function calcular() {
 
         // Calculo valor 30%
 
-        var a9 = document.getElementById("preco").value
-        var b9 = document.getElementById("preco").value * 30 / 100;
+        var a9 = document.getElementById("campo3").value
+        var b9 = document.getElementById("campo3").value * 30 / 100;
         var re9 = a9 - b9;
-        var c9 = re9 - (re9 * (0.0925 + document.getElementById("icms").value / 100));
-        var d9 = c9 - document.getElementById("custo").value;
+        var c9 = re9 - (re9 * (0.0925 + document.getElementById("campo1").value / 100));
+        var d9 = c9 - document.getElementById("campo2").value;
         var e9 = d9 / c9 * 100;
 
 
@@ -217,11 +217,11 @@ function calcular() {
 
         // Calculo valor 33%
 
-        var a10 = document.getElementById("preco").value
-        var b10 = document.getElementById("preco").value * 33 / 100;
+        var a10 = document.getElementById("campo3").value
+        var b10 = document.getElementById("campo3").value * 33 / 100;
         var re10 = a10 - b10;
-        var c10 = re10 - (re10 * (0.0925 + document.getElementById("icms").value / 100));
-        var d10 = c10 - document.getElementById("custo").value;
+        var c10 = re10 - (re10 * (0.0925 + document.getElementById("campo1").value / 100));
+        var d10 = c10 - document.getElementById("campo2").value;
         var e10 = d10 / c10 * 100;
 
 
@@ -241,11 +241,11 @@ function calcular() {
 
         // Calculo valor 35%
 
-        var a11 = document.getElementById("preco").value
-        var b11 = document.getElementById("preco").value * 35 / 100;
+        var a11 = document.getElementById("campo3").value
+        var b11 = document.getElementById("campo3").value * 35 / 100;
         var re11 = a11 - b11;
-        var c11 = re11 - (re11 * (0.0925 + document.getElementById("icms").value / 100));
-        var d11 = c11 - document.getElementById("custo").value;
+        var c11 = re11 - (re11 * (0.0925 + document.getElementById("campo1").value / 100));
+        var d11 = c11 - document.getElementById("campo2").value;
         var e11 = d11 / c11 * 100;
 
 
@@ -264,11 +264,11 @@ function calcular() {
 
         // Calculo valor 40%
 
-        var a12 = document.getElementById("preco").value
-        var b12 = document.getElementById("preco").value * 40 / 100;
+        var a12 = document.getElementById("campo3").value
+        var b12 = document.getElementById("campo3").value * 40 / 100;
         var re12 = a12 - b12;
-        var c12 = re12 - (re12 * (0.0925 + document.getElementById("icms").value / 100));
-        var d12 = c12 - document.getElementById("custo").value;
+        var c12 = re12 - (re12 * (0.0925 + document.getElementById("campo1").value / 100));
+        var d12 = c12 - document.getElementById("campo2").value;
         var e12 = d12 / c12 * 100;
 
 
@@ -287,11 +287,11 @@ function calcular() {
 
         // Calculo valor 45%
 
-        var a13 = document.getElementById("preco").value
-        var b13 = document.getElementById("preco").value * 45 / 100;
+        var a13 = document.getElementById("campo3").value
+        var b13 = document.getElementById("campo3").value * 45 / 100;
         var re13 = a13 - b13;
-        var c13 = re13 - (re13 * (0.0925 + document.getElementById("icms").value / 100));
-        var d13 = c13 - document.getElementById("custo").value;
+        var c13 = re13 - (re13 * (0.0925 + document.getElementById("campo1").value / 100));
+        var d13 = c13 - document.getElementById("campo2").value;
         var e13 = d13 / c13 * 100;
 
 
@@ -310,11 +310,11 @@ function calcular() {
 
         // Calculo valor 50%
 
-        var a14 = document.getElementById("preco").value
-        var b14 = document.getElementById("preco").value * 50 / 100;
+        var a14 = document.getElementById("campo3").value
+        var b14 = document.getElementById("campo3").value * 50 / 100;
         var re14 = a14 - b14;
-        var c14 = re14 - (re14 * (0.0925 + document.getElementById("icms").value / 100));
-        var d14 = c14 - document.getElementById("custo").value;
+        var c14 = re14 - (re14 * (0.0925 + document.getElementById("campo1").value / 100));
+        var d14 = c14 - document.getElementById("campo2").value;
         var e14 = d14 / c14 * 100;
 
 
@@ -333,11 +333,11 @@ function calcular() {
 
         // Calculo valor 60%
 
-        var a15 = document.getElementById("preco").value
-        var b15 = document.getElementById("preco").value * 60 / 100;
+        var a15 = document.getElementById("campo3").value
+        var b15 = document.getElementById("campo3").value * 60 / 100;
         var re15 = a15 - b15;
-        var c15 = re15 - (re15 * (0.0925 + document.getElementById("icms").value / 100));
-        var d15 = c15 - document.getElementById("custo").value;
+        var c15 = re15 - (re15 * (0.0925 + document.getElementById("campo1").value / 100));
+        var d15 = c15 - document.getElementById("campo2").value;
         var e15 = d15 / c15 * 100;
 
 
@@ -456,3 +456,18 @@ function limpar() {
     document.getElementById("li4").style.backgroundColor = "#ffffff";
 
 }
+
+$(document).ready(function () {
+    $('.pula').keypress(function (e) {
+        var tecla = (e.keyCode ? e.keyCode : e.which);
+        if (tecla == 13) {
+            campo = $('.pula');
+            indice = campo.index(this);
+            if (campo[indice + 1] != null) {
+                proximo = campo[indice + 1];
+                proximo.focus();
+            }
+        }
+       
+    })
+})
